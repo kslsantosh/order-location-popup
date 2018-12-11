2953b80c6cf48a7a799def9cbcbdbf394ffae69e
Santosh Kanakagiri <santosh.k@licious.in>
2018-12-11 17:39:21 +0530

    initial project templete

---
+++ src/main/java/com/santosh/kafka/orderlocationpopup/OrderLocationPopupApplication.java
@@ -0,0 +1,12 @@
+package com.santosh.kafka.orderlocationpopup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderLocationPopupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderLocationPopupApplication.class, args);
+	}
+}
---
+++ src/test/java/com/santosh/kafka/orderlocationpopup/OrderLocationPopupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.santosh.kafka.orderlocationpopup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderLocationPopupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
